c1ce9742a88f00b60689f090127408065b98a8a2
Victor <victor@localhost.localdomain>
2020-10-14 02:52:21 -0300

    Adicionado as funções: IsFull, CreateStack, DestroyStack, Size e SetSize

--- src/testa_pilha.c
+++ src/testa_pilha.c
@@ -1,20 +1,12 @@
-// calc_test.cpp
 #include "gtest/gtest.h"
 #include "pilha.h"
 
-TEST(CalcTest, Add) {
-    ASSERT_EQ(2, add(1, 1));
-    ASSERT_EQ(5, add(3, 2));
-    ASSERT_EQ(10, add(7, 3));
-}
+#define TRUE 1
+#define FALSE 0
 
-TEST(CalcTest, Sub) {
-    ASSERT_EQ(3, sub(5, 2));
-    ASSERT_EQ(-10, sub(5, 15));
-}
-
+//TESTES DE PILHA VAZIA
 TEST(TestePilha, IsEmpty) {
-    Pilha pil  = (Pilha) malloc(sizeof(pilha));
+    pilha pil  = (pilha) malloc(sizeof(plha));
     //printf("%p\n",pil->elemento);
     
     //printf("%p\n",pil->elemento);
@@ -29,7 +21,86 @@ TEST(TestePilha, IsEmpty) {
     //QUANDO PILHA POSSUE PELO MENOS UM ELEMENTO
     ASSERT_EQ(1, isEmpty(pil));
 
+    free(pil->elemento);
+    free(pil);
 }
+
+
+//TESTES PARA PILHA CHEIA
+TEST(IsFull, False){
+    pilha p  = (pilha) malloc(sizeof(plha));
+    p->elemento = NULL;
+    //printf("---=>%i", p->elemento);
+    ASSERT_EQ(0, isFull(p));
+    free(p);
+}
+
+TEST(IsFull, True){
+    pilha pil  = (pilha) malloc(sizeof(plha));
+    pil->elemento  = (Elemento) malloc(sizeof(Elemento));
+    //pil->elemento->item = 42;
+
+    ASSERT_EQ(TRUE, isFull(pil));
+
+    free(pil->elemento);
+    free(pil);
+}
+
+TEST(CreateStack, RetornaPilha){
+    pilha pil;
+    pil = createStack();
+
+    //Verifica se a pilha tem espaço para adicionar um elemento
+    ASSERT_EQ(0, isFull(pil));
+    free(pil);
+}
+
+//TESTES DE DESTRUIÇÃO DE PILHA
+TEST(DestroyStack, UnicoNo){
+    pilha pil = createStack();
+    ASSERT_EQ(TRUE, destroyStack(pil));
+}
+
+TEST(DestroyStack, N_No){
+    pilha p1 = createStack();
+    pilha p2 = createStack();
+    pilha p3 = createStack();
+    p1->prox = p2;
+    p2->prox = p3;
+    ASSERT_EQ(TRUE, destroyStack(p1));
+}
+
+TEST(Size, N_SIZE){
+    pilha p1 = createStack();
+    pilha p2 = createStack();
+    pilha p3 = createStack();
+    p1->prox = p2;
+    p2->prox = p3;
+
+    ASSERT_EQ(3, size(p1));
+}
+
+//AUMENTAR O TAMANHO DA PILHA
+TEST(SetSize, True){
+    pilha p1 = createStack();
+    pilha p2 = createStack();
+    pilha p3 = createStack();
+    p1->prox = p2;
+    p2->prox = p3;
+    ASSERT_EQ(TRUE, setSize(p1, 9));
+    ASSERT_EQ(9, size(p1));
+}
+TEST(SetSize, False){
+    //Se o tamanho escolhido for menor que o tamanho atual da pilha, o tamanho não será modificado para que não ocorra a perda de informação no final da pilha
+    pilha p1 = createStack();
+    pilha p2 = createStack();
+    pilha p3 = createStack();
+    p1->prox = p2;
+    p2->prox = p3;
+    ASSERT_EQ(FALSE, setSize(p1, 2));
+    ASSERT_EQ(3, size(p1));
+}
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
